39022a62355bb4520b572824337aaa493a2490c9
Russ Smith <russ@bashme.org>
2008-04-06 12:25:06 -0700

    Moved the repo variable to a global. Not sure if this the best practice or not.

--- lib/gitnub/branches.rb
+++ lib/gitnub/branches.rb
@@ -1,21 +1,28 @@
 module GitNub
+	# Subclass of the ComboBox model
+	# Keeps things nice and clean
 	class Branches < Gtk::ComboBox
+
+		alias :active_branch :active_text
+
 		def initialize
 			super
 			model = Gtk::ListStore.new(String)
 			populate
 			set_active(0)
+
+			# Connect the change signal to update 
+			# the application view
+			signal_connect("changed") { p "update..." }
 		end
-	
-		def active_branch
-			active_text
-		end
-	
+
 		private
-	
+
+		# Populates the Combobox with the 
+		# repos branches	
 		def populate
-			[ 'master' ].each do |branch|
-				append_text(branch)
+			$repo.branches.each do |branch|
+				append_text(branch.name)
 			end
 		end
 	end
